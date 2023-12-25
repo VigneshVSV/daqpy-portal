0c2e35635267d22c636c3cc599a99d704a75d6e2
Vignesh.Vaidyanathan <62492557+VigneshVSV@users.noreply.github.com>
2023-12-25 13:12:42 +0100

    moved reused hooks from reuse-components to separate file (2)

--- app/builtins/reuse-components.tsx
+++ app/builtins/reuse-components.tsx
@@ -1,9 +1,7 @@
+'use client'
 // Internal & 3rd party functional libraries
-import  React, { useState, useCallback, MutableRefObject } from "react";
+import { useState, useCallback } from "react";
 // Custom functional libraries
-import { asyncRequest } from "mobx-render-engine/utils/http";
-import { StateManager } from "mobx-render-engine/state-manager";
-import { fetchFieldFromLocalStorage } from "mobx-render-engine/utils/misc";
 // Internal & 3rd party component libraries
 import NewWindow from "react-new-window";
 import { Backdrop, Box, CircularProgress, IconButton, Stack, Typography } from "@mui/material"
@@ -126,7 +124,7 @@ type ErrorBackdropProps =  {
 
 export const ErrorBackdrop = ({ message, subMessage, goBack } : ErrorBackdropProps) => {
     // https://mui.com/material-ui/react-backdrop/#system-SimpleBackdrop.js
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = useState(true);
     const handleClose = useCallback(() => {
       setOpen(false);
     }, [])
@@ -159,7 +157,7 @@ export const ErrorBackdrop = ({ message, subMessage, goBack } : ErrorBackdropPro
 
 export const LoadingBackdrop = ({ message, goBack } : ErrorBackdropProps) => {
     // https://mui.com/material-ui/react-backdrop/#system-SimpleBackdrop.js
-    const [open, setOpen] = React.useState(true);
+    const [open, setOpen] = useState(true);
     const handleClose = useCallback(() => {
       setOpen(false);
     }, [])
@@ -223,95 +221,4 @@ export const RenderInWindow = (props : any) => {
 
 
 
-export const useDashboard = (dashboardURL : string, dashboardStateManager : MutableRefObject<StateManager | null>) : [
-        loading : boolean, 
-        errorMessage : string, 
-        errorTraceback : string[],
-        fetchData : any
-    ] => {
-    
-    const [loading, setLoading] = useState<boolean>(false)
-    const [errorMessage, setErrorMessage] = useState<string>('')
-    const [errorTraceback, setErrorTraceback] = useState<string[]>([]) 
 
-    const fetchData = useCallback(async() => {
-        let errMsg = '', errTb = []
-        setLoading(true)
-        const response : any = await asyncRequest({
-            url : dashboardURL,
-            method : 'get'
-        })
-        if (!dashboardStateManager.current){
-            errMsg = 'Internal error - dashboard state manager not created. Use setDashboardStateManager hook before calling fetchData'
-        }
-        else if(response.status === 200) {
-            try {
-                dashboardStateManager.current.deleteComponents()
-                dashboardStateManager.current.deleteActions()
-                dashboardStateManager.current.store(
-                    dashboardURL,
-                    response.data.returnValue.UIcomponents, 
-                    response.data.returnValue.actions
-                )
-                dashboardStateManager.current.updateActions(response.data.returnValue.actions)
-                dashboardStateManager.current.updateComponents(response.data.returnValue.UIcomponents)
-            } catch (error) {
-                errMsg = "Failed to load view - " + error   
-                dashboardStateManager.current.logger.logErrorMessage("IconButton", "quick-view", error as string)      
-            }
-        }
-        else if(response.data.exception) {
-            errMsg = response.data.exception.message
-            errTb = response.data.exception.traceback
-        }
-        else {
-            console.log("dashboard fetch failed - ", response)
-            let reason = response.status ? `resonse status code - ${response.status}` : 'invalid response after request - is the address correct?'
-            errMsg = `Failed to fetch JSON - ${reason}`
-            dashboardStateManager.current.logger.logErrorMessage("IconButton", "quick-view", reason)  
-        }
-        setLoading(false)
-        setErrorMessage(errMsg)
-        setErrorTraceback(errTb)
-        if(response.status && response.status === 200 && !errMsg)
-            return true 
-        return false
-    }, [dashboardURL])
-    return [loading, errorMessage, errorTraceback, fetchData]
-}
-
-
-
-export const useAutoCompleteOptionsFromLocalStorage = (field : string) => {
-    const [existingData, setExistingData] = useState<{[key : string] : any}>(fetchFieldFromLocalStorage(null, {}))
-    if(!existingData[field])
-        existingData[field] = [] // no need to re-render - it will correct at first iteration
-    
-    const modifyOptions = useCallback((entry : string, operation : 'ADD' | 'DELETE') => {
-        if(operation === 'ADD') {
-            if(!existingData[field].includes(entry)) 
-                existingData[field].push(entry)
-        }
-        else {
-            if(existingData[field].includes(entry)) {
-                existingData[field].splice(existingData[field].indexOf(entry), 1)
-            }
-        }
-        setExistingData(existingData)
-        localStorage.setItem('daqpy-webdashboard', JSON.stringify(existingData))
-    }, [existingData])
-    return [existingData[field], modifyOptions]
-}   
-
-
-
-// https://github.com/CharlesStover/use-force-update
-const createNewObject = (): Record<string, never> => ({});
-
-export default function useForceUpdate(): [Record<string, never>, VoidFunction] {
-    const [useEffectDummyDependent, setValue] = useState<Record<string, never>>(createNewObject());
-
-    return [useEffectDummyDependent, useCallback((): void => {
-        setValue(createNewObject());
-    }, [])]
-}
